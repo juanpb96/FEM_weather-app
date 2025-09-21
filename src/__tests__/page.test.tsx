0b1d79b0ddeab25f410819d5214801d9567e7d76
MasterDev <juanpb.96@gmail.com>
2025-09-21 15:06:55 -0500

    feat(3): Include vitest and test script
    Add vitest for unit testing
    
    Add missing test script for CI workflow to run properly

---
+++ src/__tests__/page.test.tsx
@@ -0,0 +1,8 @@
+import { expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Page from "../app/page";
+
+test("Logo should be present", () => {
+  render(<Page />);
+  expect(screen.getByRole("img", { name: "Next.js logo" })).toBeDefined();
+});
