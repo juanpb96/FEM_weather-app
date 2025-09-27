e7a61547068a4151386a1bf7a814a5bad6746eb9
MasterDev <juanpb.96@gmail.com>
2025-09-26 20:56:22 -0500

    Make basePath and assetPrefix match with the test path

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "",
-  assetPrefix: "/",
+  basePath: "/FEM_weather-app-test",
+  assetPrefix: "/FEM_weather-app-test",
 };
 
 export default nextConfig;
