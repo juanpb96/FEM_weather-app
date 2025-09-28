e4a9f38fd0833ffffd33076f1d1f30ef4dffee1c
MasterDev <juanpb.96@gmail.com>
2025-09-28 15:48:34 -0500

    feat(11): Add Edge Function for geolocation
    Add edge function to run for path / and update request headers to include geolocation data
    
    Install netlify edge functions to extract types
    
    Add a HEADERS constant to keep custom geolocation headers centralized

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,10 @@
 export const BASE_PATH = process.env.WEATHER_APP_PATH || "";
+
+const HEADER_PREFIX = "geo";
+
+export const GEO_HEADERS = {
+  country: `${HEADER_PREFIX}-country`,
+  city: `${HEADER_PREFIX}-city`,
+  latitude: `${HEADER_PREFIX}-latitude`,
+  longitude: `${HEADER_PREFIX}-longitude`,
+} as const;
