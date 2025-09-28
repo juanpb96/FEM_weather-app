817a6c797c7dc4affb063901b4697c01f6645779
MasterDev <juanpb.96@gmail.com>
2025-09-28 15:51:20 -0500

    feat(11): Get geolocation data in Next
    Extract geolocation data from custom headers
    
    Get precise user's location using the Geolocation API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,28 @@ import { Header } from "@/components/Header";
 import { HourlyForecast } from "@/components/HourlyForecast";
 import { LocationSearchBar } from "@/components/LocationSearchBar";
 import { TodayOverview } from "@/components/TodayOverview";
+import { GEO_HEADERS } from "@/constants";
+import { headers } from "next/headers";
 
-export default function Home() {
+export default async function Home() {
+  const nextHeaders = await headers();
+  const country = nextHeaders.get(GEO_HEADERS.country) || "";
+  const city = nextHeaders.get(GEO_HEADERS.city) || "";
+  const latitude = nextHeaders.get(GEO_HEADERS.latitude);
+  const longitude = nextHeaders.get(GEO_HEADERS.longitude);
+
   return (
     <div>
       <Header />
       <h1>How’s the sky looking today?</h1>
+      <div>
+        <p>Latitude: {latitude}</p>
+        <p>Longitude: {longitude}</p>
+      </div>
       <LocationSearchBar />
       <main>
         <div>
-          <TodayOverview />
+          <TodayOverview city={city} country={country} />
           <CurrentConditions />
           <DailyForecast />
         </div>
--- src/components/TodayOverview/TodayOverview.tsx
+++ src/components/TodayOverview/TodayOverview.tsx
@@ -1,8 +1,34 @@
-export function TodayOverview() {
+"use client";
+
+interface TodayOverviewProps {
+  city: string;
+  country: string;
+}
+
+function success(pos: GeolocationPosition) {
+  const { latitude, longitude } = pos.coords;
+  console.log(latitude, longitude);
+}
+
+function error(err: GeolocationPositionError) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+export function TodayOverview({ city, country }: TodayOverviewProps) {
+  function getPreciseLocation() {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+
   return (
     <div>
       <div>
-        <p>Berlin, Germany</p>
+        <p>
+          {city}, {country}
+        </p>
+        <p>
+          Not your location?{" "}
+          <button onClick={getPreciseLocation}>Provide precise location</button>
+        </p>
         <p>Tuesday, Aug 5, 2025</p>
       </div>
       <p>20°</p>
