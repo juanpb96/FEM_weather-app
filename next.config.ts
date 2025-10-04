a8f3d01889ba5c5b27c81be2a79330fd373815e3
MasterDev <juanpb.96@gmail.com>
2025-10-03 19:10:42 -0500

    chore(14): Replace _redirects file with redirect in Next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/FEM_weather-app",
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/FEM_weather-app",
+        permanent: true,
+        basePath: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
